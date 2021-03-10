dc5e84488ecf51c3a6b5e127905d845211e4e3c1
hasunhkhan <hasunkhan123@gmail.com>
2021-03-09 22:09:44 -0800

    Second Exercise utilizing fileIO.py to print out students who passed an exam and students who failed.

--- fileIO.py
+++ fileIO.py
@@ -1,8 +1,12 @@
 f = open('Exercise Files\inputFile.txt', 'r')
-count = 0
+passFile = open('PassFile.txt', 'w')
+failFile = open('FailFile.txt', 'w')
 for line in f:
     line_split = line.split()
     if line_split[2]== 'P':
-        print(line)
-
+        passFile.write(line)
+    else:
+        failFile.write(line)
 f.close()
+passFile.close()
+failFile.close()
