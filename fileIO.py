1728161b2e9f8b8c096a78ad6527a216029df602
hasunhkhan <hasunkhan123@gmail.com>
2021-03-09 22:05:27 -0800

    First Exercise utilizing fileIO.py to print out students who passed an exam.

--- fileIO.py
+++ fileIO.py
@@ -1,3 +1,8 @@
 f = open('Exercise Files\inputFile.txt', 'r')
-print(f.read())
+count = 0
+for line in f:
+    line_split = line.split()
+    if line_split[2]== 'P':
+        print(line)
+
 f.close()
